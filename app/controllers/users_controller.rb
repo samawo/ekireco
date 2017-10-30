5a477fe84274bf80c8b6561650a715e85c25fd12
tetsuyasuzuki <suzukakousen_suzuki@yahoo.co.jp>
2017-10-30 12:17:45 +0000

    user page change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     def show
         @user=User.find(params[:id])
+        @stations=@user.stations
+        @routes=@user.routes.sort{|a,b| a.id<=>b.id}
+        @prefectures=@user.prefectures.sort{|a,b| a.id<=>b.id}
     end
 end
