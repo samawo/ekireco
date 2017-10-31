0871e0418a43cd868a76f174e5414826d6ece22c
tetsuyasuzuki <suzukakousen_suzuki@yahoo.co.jp>
2017-10-31 02:38:56 +0000

    add Tweet module

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -16,8 +16,45 @@ class TwitterController < ApplicationController
       #config.access_token         = current_user.token
       #config.access_token_secret  = current_user.secret
     end
-    # Twitter投稿
-    client.update("test")
+    precount=0
+    current_user.complete_prefectures.each do |n|
+      if n.number!= 0
+        precount=precount+1
+      end
+    end
+    precomp=0
+    current_user.complete_prefectures.each do |n|
+      if n.number!= 0
+        precomp=precomp+1
+      end
+    end
+    routecount=0
+    current_user.complete_prefectures.each do |n|
+      if n.number!= 0
+        routecount=routecount+1
+      end
+    end
+    routecomp=0
+    current_user.complete_prefectures.each do |n|
+      if n.number!= 0
+        routecomp=routecomp+1
+      end
+    end
+    
+     # Twitter投稿
+    twiText="現在のステータス\nアクセス都道府県数："
+    twiText+=string(precount)
+    twiText+="\n制覇都道府県数："
+    twiText+=string(precomp)
+    twiText+="\nアクセス路線数："
+    twiText+=string(routecount)
+    twiText+="\n制覇路線数："
+    twiText+=string(routecomp)
+    twiText+="\nアクセス駅数："
+    twiText+=string(routecomp)
+    twiText+="\nhttps://station-record.herokuapp.com/users/"
+    twiText+=string(current_user.id)
+    client.update(twiText)
     redirect_to root_path, notice: 'ツイートしました！'
   end
 end
